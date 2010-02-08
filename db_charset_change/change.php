867a36b40913961538237986b74b67552e81715d
Özgür Kuru <ozgur@ozgurkuru.net>
2010-02-08 19:28:00 +0200

    new types

--- db_charset_change/change.php
+++ db_charset_change/change.php
@@ -2,7 +2,6 @@
 include('config.php');
 mysql_connect("$host","$user","$pass");
 mysql_select_db("$db");
-
 echo ">>> Changing $db's Charset... <<<\n";
 $db_charset_change_query= mysql_query("ALTER DATABASE $db DEFAULT CHARACTER SET $set COLLATE $collate");
 $tables_query = mysql_query("SHOW TABLES");
@@ -24,8 +23,8 @@ foreach($tables as $table){
         while($result=mysql_fetch_assoc($columns_query)){
                 $types = explode('(',$result[Type]);
                 $type=$types[0];
-                if($type=="char" or $type=="varchar" or $type=="longtext" or $type="text"){
-                        $query=mysql_query("ALTER TABLE `$table` CHANGE `$result[Field]` `$result[Field]` $result[Type] CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL");
+                if($type=="char" || $type=="varchar" || $type="longtext" || $type="mediumtext" || $type="text" || $type="tinytext"){
+                        $query=mysql_query("ALTER TABLE `$table` CHANGE `$result[Field]` `$result[Field]` $result[Type] CHARACTER SET $set COLLATE $collate NOT NULL");
                 }
         }
 }
